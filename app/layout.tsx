073e2d80067b63672ddce2c7cab18038cf38b9df
Tristesse02 <mtvu@umass.edu>
2023-08-19 23:34:07 -0400

    add editor config and trivial fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Operating System using TS',
-  description: 'Creating an OS using webpage',
-}
+  title: "Operating System using TS",
+  description: "Creating an OS using webpage",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
