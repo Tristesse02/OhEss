e63904f261ac3e239fc27cfb8bdb2a3549171b91
Tristesse02 <mtvu@umass.edu>
2023-08-20 14:28:13 -0400

    Ts support (already added)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import './styles.css';
+import 'app/styles.css';
 
 export default function Home() {
   return (
@@ -10,3 +9,4 @@ export default function Home() {
     </>
   );
 }
+
