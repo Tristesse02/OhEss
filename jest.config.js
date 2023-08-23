6b2b024c836670a5c9ee1bcec602df611ed5b292
Tristesse02 <mtvu@umass.edu>
2023-08-23 07:19:49 -0400

    remove unnecessary trails

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   moduleDirectories: ['node_modules', '<rootDir>'],
   moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': 'identity-obj-proxy'
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
-  },
+    '^.+\\.(ts|tsx)$': 'babel-jest'
+  }
 };
