1a07aa38ee7ad330460f973749df40955d0eccdd
Tristesse02 <mtvu@umass.edu>
2023-08-23 07:17:32 -0400

    remove scss & integrate style components

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import styled from "styled-components";
 const Title = styled.h1`
   font-size: 3.5em;
   text-align: center;
-  color: #db7093;
+  color: ${({ theme }) => theme.colors.primary};
 `;
 
 export default function Home() {
