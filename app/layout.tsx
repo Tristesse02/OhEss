c34412dc515eb2cbbed6b45de839ab63619b4c6f
Tristesse02 <mtvu@umass.edu>
2023-08-22 22:28:25 -0400

    Customized? HTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import type { Metadata } from "next";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Operating System using TS",
+  title: "OhEss",
   description: "Creating an OS using webpage",
 };
 
