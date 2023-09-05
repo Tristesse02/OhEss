70ed16d8d5b12dc4c9d04dbb79357b1df67ad730
Tristesse02 <mtvu@umass.edu>
2023-09-05 08:06:21 -0400

    Renaming WindowMng to ProcessLdr

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import WindowManager from "components/system/WindowManager";
+import ProcessLoader from "components/system/ProcessLoader";
 import { ProcessProvider } from "contexts/process";
 import React from "react";
 
 const Home = (): React.JSX.Element => {
   return (
     <ProcessProvider>
-      <WindowManager />
+      <ProcessLoader />
     </ProcessProvider>
   );
 };
