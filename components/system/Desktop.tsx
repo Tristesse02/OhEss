a47ca19d47d474bc8011276d2175b9736d712009
Tristesse02 <mtvu@umass.edu>
2024-01-10 22:44:39 -0500

    fix linting problem

--- components/system/Desktop.tsx
+++ components/system/Desktop.tsx
@@ -1,6 +1,7 @@
+import React from "react";
+import type { FC } from "react";
 import StyledDesktop from "styles/components/system/StyledDesktop";
-import { FC } from "react";
-import { DesktopProps } from "types/components/pages/Desktop";
+import type { DesktopProps } from "types/components/pages/Desktop";
 
 const Desktop: FC<DesktopProps> = ({ children }) => (
   <StyledDesktop>{children}</StyledDesktop>
