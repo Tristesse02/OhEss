e86604e6c8a511866a5fa111d9caa406b0eb8252
Tristesse02 <mtvu@umass.edu>
2024-01-10 22:51:04 -0500

    fix linting problem

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ProcessProvider } from "contexts/process";
 import ProcessLoader from "components/system/ProcessLoader";
 import { getStartupProcesses } from "utils/processDirectory";
-import Desktop from "@/components/system/Desktop";
+import Desktop from "components/system/Desktop";
 
 const Home = (): React.JSX.Element => {
   return (
