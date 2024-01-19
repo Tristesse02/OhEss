4406123e807a326e4bacb437b32f0376268c63cc
Tristesse02 <mtvu@umass.edu>
2024-01-19 08:57:59 -0500

    Switch ProcessesReducer to  renderProcess function

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import RenderProcess from "./RenderProcess";
 import { ProcessConsumer } from "contexts/process";
-import type { Process } from "types/contexts/process";
+import type { Processes } from "types/contexts/process";
 
-const ProcessReducer = ([id, process]: [string, Process]): JSX.Element => (
-  <RenderProcess key={id} {...process} />
-);
+const renderProcesses = (processes: Processes) =>
+  Object.entries(processes).map(([id, process]) => (
+    <RenderProcess key={id} {...process} />
+  ));
 
 const ProcessLoader = (): JSX.Element => (
   <ProcessConsumer>
-    {({ processes }) => Object.entries(processes).map(ProcessReducer)}
+    {({ processes }) => renderProcesses(processes)}
   </ProcessConsumer>
 );
 
