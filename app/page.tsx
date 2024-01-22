5a66c744ae8422cd6123767f04a15014acee13c7
Tristesse02 <mtvu@umass.edu>
2024-01-22 04:41:00 -0500

    Playing around with FileSystemProvider and Consumer

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,19 @@ import Desktop from "components/system/Desktop";
 import { ProcessProvider } from "contexts/process";
 import ProcessLoader from "components/system/Processes/ProcessLoader";
 import Taskbar from "components/system/Taskbar";
+import { FileSystemProvider } from "contexts/fileSystem";
 
 const Home = (): React.JSX.Element => {
   return (
     <React.StrictMode>
-      <Desktop>
-        <ProcessProvider>
-          <Taskbar />
-          <ProcessLoader />
-        </ProcessProvider>
-      </Desktop>
+      <FileSystemProvider>
+        <Desktop>
+          <ProcessProvider>
+            <Taskbar />
+            <ProcessLoader />
+          </ProcessProvider>
+        </Desktop>
+      </FileSystemProvider>
     </React.StrictMode>
   );
 };
