af3a3c13c561387ba02a9d2cea8ca2de2fb14222
Tristesse02 <mtvu@umass.edu>
2024-01-22 23:10:18 -0500

    Don't allow user select

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -12,7 +12,8 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    user-Select: none;
   }
 
   button {
