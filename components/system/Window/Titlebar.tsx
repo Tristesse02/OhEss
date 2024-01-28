4ba0e67a4c0a254c504116edfcff229acd548ed1
Tristesse02 <mtvu@umass.edu>
2024-01-28 11:18:06 -0500

    Styled titlebar

--- components/system/Window/Titlebar.tsx
+++ components/system/Window/Titlebar.tsx
@@ -34,7 +34,7 @@ const Titlebar = ({ id }: TitlebarProps): JSX.Element => {
         <Button onClick={onMaximize}>
           <MaximizeIcon />
         </Button>
-        <Button onClick={onClose}>
+        <Button className="close" onClick={onClose}>
           <CloseIcon />
         </Button>
       </nav>
