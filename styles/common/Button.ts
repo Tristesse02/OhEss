970e928f4dfd2b1355ed599fa32772d6e059a7a6
Tristesse02 <mtvu@umass.edu>
2024-01-28 11:18:19 -0500

    Adding property to Button

--- styles/common/Button.ts
+++ styles/common/Button.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Button = styled.button.attrs(() => ({ type: 'button' }))`
   background-color: transparent;
   font-family: inherit;
+  width: 100%;
 `;
 
 export default Button;
