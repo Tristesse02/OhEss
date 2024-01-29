aecceeb973f5b57d01457d42fcdefffeeabe05a0
Tristesse02 <mtvu@umass.edu>
2024-01-29 08:38:28 -0500

    Remove React.FC

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -7,9 +7,11 @@ import useDraggableAndResizable from "hooks/useDraggableAndResizable";
 import StyledWindow from "styles/components/system/Window/StyledWindow";
 import type { ProcessComponentProps } from "../Processes/RenderProcess";
 
-const Window: React.FC<
-  ProcessComponentProps & { children: React.ReactNode }
-> = ({ id, children }): JSX.Element => {
+interface WindowProps extends ProcessComponentProps {
+  children: React.ReactNode;
+}
+
+const Window = ({ id, children }: WindowProps): JSX.Element => {
   const {
     processes: {
       [id]: { maximized, minimized }
--- utils/contextFactory.tsx
+++ utils/contextFactory.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 
-export interface ContextProviderProps {
+interface ContextProviderProps {
   children: React.ReactNode;
 }
 
@@ -9,7 +9,7 @@ export type ContextFactory = <T>(
   useContextState: () => T
 ) => {
   Consumer: React.Consumer<T>;
-  Provider: React.FC<ContextProviderProps>;
+  Provider: (props: ContextProviderProps) => JSX.Element;
   useContext: () => T;
 };
 
@@ -20,11 +20,7 @@ export const contextFactory: ContextFactory = <T,>(
   const Context = createContext<T>(initialContextState);
 
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const ContextProvider = ({
-    children
-  }: {
-    children: React.ReactNode;
-  }): JSX.Element => (
+  const ContextProvider = ({ children }: ContextProviderProps): JSX.Element => (
     // eslint-disable-next-line react/react-in-jsx-scope
     <Context.Provider value={useContextState()}>{children}</Context.Provider>
   );
